4a99e57087ea5658f1d94cef896fcdd8728f9a1b
泛舟 <732598668@qq.com>
2021-05-26 17:38:59 +0800

    first commit

---
+++ pages/message/message.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		sdkjhfskjdhfj
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
