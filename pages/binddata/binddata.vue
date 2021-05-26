fdc5a4712bffa88ca15ff09d4754486294a2bad5
泛舟 <732598668@qq.com>
2021-05-26 19:40:10 +0800

    “在uniapp中使用定义data数据

---
+++ pages/binddata/binddata.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view class="title">
+			<text>{{ title }}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: "我是标题"
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.title{
+	font-size: 40rpx;
+	font-weight: bold;
+}
+</style>
