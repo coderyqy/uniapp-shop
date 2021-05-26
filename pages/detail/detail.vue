8beb0b1086d9bd286dacbd24d1d6c04b5d31f410
泛舟 <732598668@qq.com>
2021-05-26 18:55:46 +0800

    熟悉基本的配置

---
+++ pages/detail/detail.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		dfgdfg
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
